1048ec3df990c0ae0239559046c505cb8d120056
jmonfu <jmonfu@gmail.com>
2024-12-01 13:11:54 +0100

    removed the unecessary app from this repo

--- app/types/index.ts
+++
@@ -1,5 +0,0 @@
-export interface Project {
-  title: string;
-  description: string;
-  // Add other project properties you need
-}
--- app/types/json.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "*.json" {
-    const value: {
-        subjects: Array<{ id: string; name: string }>;
-        complexity: Array<{ id: string; name: string }>;
-    };
-    export default value;
-} 
