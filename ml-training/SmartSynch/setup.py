f6548b93fda8da080022e3df56464edc4f96958f
jmonfu <jmonfu@gmail.com>
2024-12-06 16:41:20 +0100

    new deploy

--- ml-training/SmartSynch/setup.py
+++ ml-training/SmartSynch/setup.py
@@ -1,16 +1,17 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_packages
 
 setup(
     name="smartsynch",
     version="1.0.0",
-    packages=find_namespace_packages(include=["smartsynch*"]),
+    packages=find_packages(),
     install_requires=[
-        "fastapi",
-        "uvicorn",
+        "fastapi==0.115.5",
+        "uvicorn==0.32.1",
         "python-dotenv",
         "transformers",
         "torch",
         "sentence-transformers",
-        "huggingface-hub"
+        "huggingface-hub<1.0,>=0.23.2",
+        "pydantic==2.10.2"
     ]
 )
