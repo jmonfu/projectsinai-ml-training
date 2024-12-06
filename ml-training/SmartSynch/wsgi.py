71bf2d391d1029f6604e195ef819bfb720dcf538
jmonfu <jmonfu@gmail.com>
2024-12-06 15:57:52 +0100

    new deploy

--- ml-training/SmartSynch/wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-# Add the project root to Python path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, current_dir)
-
-from smartsynch.api.main import app
