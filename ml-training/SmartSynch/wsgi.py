2263fca15946f7c660b311deaa34c9959f08ed2c
jmonfu <jmonfu@gmail.com>
2024-12-06 15:50:40 +0100

    new render and wsgi

--- ml-training/SmartSynch/wsgi.py
+++ ml-training/SmartSynch/wsgi.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 # Add the project root to Python path
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, current_dir)
 
 from smartsynch.api.main import app
